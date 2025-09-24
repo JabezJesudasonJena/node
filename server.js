9c99cd7ea265d62e33343c5aa2c72692b169c79c
JabezJesudasonJena <boycalledjabez@gmail.com>
2025-09-24 20:19:30 +0530

    Added all the files !

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-console.log('====================================');
-console.log("Hello World ");
-console.log('====================================');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+    res.send("This is Jabez from the server ! ")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+ 
