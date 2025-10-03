a50140f4c5583eb327b5338c3fccb434c44ddf49
JabezJesudasonJena <170162059+JabezJesudasonJena@users.noreply.github.com>
2025-10-03 15:39:50 +0530

    Updated Blogs/pages PC

--- blog_express/data/blogs.js
+++ blog_express/data/blogs.js
@@ -1,22 +1,24 @@
-blogs = {
-    blogs: [
+blogs =  [
         {
             title : "How to get started with Python ! ",
-            content : "This is a content"
+            content : "This is a content",
+            slug : "python-learn"
         },
         {
             title : "How to get started with JS ! ",
-            content : "This is a content"
+            content : "This is a content",
+            slug : "js-learn"
         },
         {
             title : "How to get started with Java ! ",
-            content : "This is a content Java"
+            content : "This is a content Java",
+            slug : "java-learn" 
         },
         {
             title : "How to get started with CPP  ! ",
-            content : "This is a content CPP "
+            content : "This is a content CPP ",
+            slug : "cpp-learn"
         }
     ]
-}
 
 module.exports = blogs;
--- blog_express/routes/blog.js
+++ blog_express/routes/blog.js
@@ -9,10 +9,20 @@ router.get('/', (req, res) => {
 })
 
 router.get('/blog', (req, res) => {
+    //console.log(blogs)
     blogs.forEach(e =>  {
         console.log(e.title)
     });
-    res.sendFile(path.join(__dirname, '../templates/index.html'))
+    res.sendFile(path.join(__dirname, "../templates/bloghome.html"))
+})
+
+router.get('/blogpost/:slug', (req, res) => {
+    //console.log(req.params.slug)
+    myBlog = blogs.filter((e) => {
+        return e.slug == req.params.slug;
+    })
+    console.log(myBlog)
+    res.sendFile(path.join(__dirname, "../templates/blogpage.html"))
 })
 
 module.exports = router
